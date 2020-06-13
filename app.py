df2949a08ed3fb4659a3b647ad5c1d86a0071d87
ThaiSonNguyenTl <45363040+ThaiSonNguyenTl@users.noreply.github.com>
2020-06-13 16:26:29 +0700

    add home index

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 
 mlab.connect()
 
+@app.route('/')
+def index():
+    allproduct = Orderproduct.objects()
+    indexnum = 0 
+    for product in allproduct:
+        indexnum += 1
+    return render_template("index.html",indexnum=indexnum)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
