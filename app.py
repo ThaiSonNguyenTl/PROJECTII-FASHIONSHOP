540627fcbb937dfab264db318cefe19c3c491120
maianhtuan98 <thansamthor1412@gmail.com>
2020-06-14 23:06:07 +0700

    fix-womanpage

--- app.py
+++ app.py
@@ -15,24 +15,17 @@ def index():
 
 @app.route('/woman')
 def woman():
-    allproduct = Orderproduct.objects()
-    indexnum = 0 
-    for product in allproduct:
-        indexnum += 1
     # get all document from dabase
     all_clother = Clothers.objects()
-    return render_template("woman.html",all_clother= all_clother,indexnum=indexnum)
+    return render_template("woman.html",all_clother= all_clother)
 
 
 @app.route('/womandetail/<womanid>', methods = ["GET","POST"])
 def womandetail(womanid):
-    allproduct = Orderproduct.objects()
-    indexnum = 0 
-    for product in allproduct:
-        indexnum += 1
+
     woman_id = Clothers.objects.with_id(womanid)
     if request.method == "GET":
-        return render_template("womandetail.html",woman_id  = woman_id,indexnum=indexnum )
+        return render_template("womandetail.html",woman_id  = woman_id )
     else:
         form = request.form
         orderproduct = Orderproduct(
